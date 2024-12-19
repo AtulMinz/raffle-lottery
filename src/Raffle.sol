2e21b3c91524dd7fe38d76cbb6859f7382e6e12a
AtulMinz <atulminz.jsr@gmail.com>
2024-12-19 23:58:37 +0530

    Updated code

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,19 @@
-//SPDX-License Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.26;
 
-contract Raffle {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus {
+    //Errors
+    error Raffle_SendToEnterRaffle();
+
+    constructor(address vrfCoordinatorV2) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+
+    }
     
+    function enterRaffle() external payable {
+
+    }
 }
